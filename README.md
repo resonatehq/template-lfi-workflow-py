3f47ee0011a341d81b09ec84b6705fea37f2ae9a
Cully Wakelin <cully.wakelin@resonatehq.io>
2025-02-17 17:14:11 -0700

    basic 2 step lfi workflow for project templates

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# scaffold-two-step-workflow-py
+# Two step local function invocation workflow template
+
+This repository serves serves as a template to scaffold Resonate projects in Python.
+
+You can use this template by installing the Resonate CLI:
+
+```
+brew install resonatehq/tap/resonate
+```
+
+Then use the project creation command and specify this template:
+
+```
+resonate project create --name <your-project-name> --template two-step-lfi-workflow
+```
+
+Check the `pyproject.toml` file to see which package versions this template is pinned to.
---
+++ src/app.py
@@ -0,0 +1,38 @@
+from resonate import Resonate
+from resonate.stores import LocalStore
+
+# Create a Resonate instance with a local store
+resonate = Resonate(store=LocalStore())
+
+
+# Register the top-level function with Resonate
+@resonate.register
+def foo(ctx, greeting):
+    print("running foo")
+    greeting = yield ctx.lfc(bar, greeting)
+    greeting = yield ctx.lfc(baz, greeting)
+    return greeting
+
+
+def bar(_, v):
+    print("running bar")
+    return f"{v} world"
+
+
+def baz(_, v):
+    print("running baz")
+    return f"{v}!"
+
+
+# Define a main function to start the Flask app
+def main():
+    try:
+        handle = foo.run("hello-world-greeting", greeting="hello")
+        print(handle.result())
+    except Exception as e:
+        print(e)
+
+
+# Run the main function when the script is invoked
+if __name__ == "__main__":
+    main()
